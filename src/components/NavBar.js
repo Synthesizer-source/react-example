84c1a888f723c25a535d64a4c8c0b85aa0e69e3c
Synthesizer-source <hsn.ozdmr.1997@gmail.com>
2021-01-16 19:54:12 +0300

    4 files modified

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,21 +13,47 @@ function NavBarItems(index) {
 
 function NavBar() {
   return (
-    <nav class="navbar navbar-expand-sm navbar-dark  text-center d-flex justify-content-center">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div class="navbar-header">
-              <a class="navbar-brand brand-text-color" href="#">
-                <span className="brand-text-color">DeveloperX</span>
-              </a>
-            </div>
-          </div>
-          <div className="col">
-            <ul class="navbar-nav">
-              <NavBarItems />
-            </ul>
-          </div>
+    // <nav class="collapse navbar-collapse navbar-expand-sm navbar-dark  text-center d-flex justify-content-center">
+    //   <div className="container">
+    //     <div className="row">
+    //       <div className="col">
+    //         <div class="navbar-header">
+    //           <a class="navbar-brand brand-text-color" href="#">
+    //             <span className="brand-text-color">DeveloperX</span>
+    //           </a>
+    //         </div>
+    //       </div>
+    //       <div className="col">
+    //         <ul class="navbar-nav">
+    //           <NavBarItems />
+    //         </ul>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </nav>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand brand-text-color" href="#">
+            <span className="brand-text-color">DeveloperX</span>
+          </a>
+        </div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <NavBarItems />
+          </ul>
         </div>
       </div>
     </nav>

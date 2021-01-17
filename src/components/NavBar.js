2488bb319df1b30b19751eb86b122cc7f623001a
Synthesizer-source <hsn.ozdmr.1997@gmail.com>
2021-01-17 14:56:43 +0300

    imported react-bootstrap and changed NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,62 +1,33 @@
 import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
 const navItems = ["Home", "About", "Instructors", "Solutions", "Collaborators"];
 
 function NavBarItems(index) {
   return navItems.map(item => (
-    <li class="nav-item active">
-      <a class="nav-link" href={"#" + item.toLowerCase()}>
-        {item}
-      </a>
-    </li>
+    // <li class="nav-item active">
+    //   <a class="nav-link" href={"#" + item.toLowerCase()}>
+    //     {item}
+    //   </a>
+    // </li>
+    <Nav.Link className="text-light" href={"#" + item.toLowerCase()}>
+      {item}
+    </Nav.Link>
   ));
 }
 
 function NavBar() {
   return (
-    // <nav class="collapse navbar-collapse navbar-expand-sm navbar-dark  text-center d-flex justify-content-center">
-    //   <div className="container">
-    //     <div className="row">
-    //       <div className="col">
-    //         <div class="navbar-header">
-    //           <a class="navbar-brand brand-text-color" href="#">
-    //             <span className="brand-text-color">DeveloperX</span>
-    //           </a>
-    //         </div>
-    //       </div>
-    //       <div className="col">
-    //         <ul class="navbar-nav">
-    //           <NavBarItems />
-    //         </ul>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </nav>
-
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand brand-text-color" href="#">
-            <span className="brand-text-color">DeveloperX</span>
-          </a>
-        </div>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon" />
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            <NavBarItems />
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="dark" expand="lg">
+      <Navbar.Brand className="offset-1" href="#home">
+        <span className="brand-text-color">DeveloperX</span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <NavBarItems />
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
